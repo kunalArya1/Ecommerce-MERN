40d88dfea7907c68e151f999d9a1bd1744b7c425
Nirbhay <nirbhaysingh943012@gmail.com>
2024-05-07 12:52:26 +0530

    model created

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: String,
+  },
+  { timestamps: true }
+);
+
+export const userModel = mongoose.model("user", userSchema);
