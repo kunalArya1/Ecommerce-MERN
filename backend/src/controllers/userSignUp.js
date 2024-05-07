79646b2ef66b0db7ae40048ab04d17c789c356f7
Nirbhay <nirbhaysingh943012@gmail.com>
2024-05-07 12:46:28 +0530

    created router and controller

---
+++ backend/src/controllers/userSignUp.js
@@ -0,0 +1,10 @@
+async function userSignUpController(req, res) {
+  try {
+  } catch (error) {
+    res.json({
+      mesaage: error,
+      error: true,
+      success: false,
+    });
+  }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import router from "./routes/routes.js";
 const app = express();
 app.use(cors());
 dotenv.config();
@@ -17,3 +18,5 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+
+// app.use("/", router);
