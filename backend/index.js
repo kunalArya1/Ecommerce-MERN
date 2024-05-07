f040406ae2669c1d6616b9021d6f0f656e915864
Nirbhay <nirbhaysingh943012@gmail.com>
2024-05-07 10:18:09 +0530

    mongodb connected

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("mongodb connected");
+    app.listen(PORT, () => {
+      console.log(`server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
