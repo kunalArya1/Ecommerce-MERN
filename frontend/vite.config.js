3b6623021b342bf708b7d16e23890e2ec8a9ee0e
Nirbhay <nirbhaysingh943012@gmail.com>
2024-08-18 16:39:39 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:7000",
+        target: "https://backend-ecommerce-ecru.vercel.app",
         changeOrigin: true,
       },
     },
