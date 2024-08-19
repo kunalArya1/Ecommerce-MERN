763d97a636997b9981cac2b4a950934d7c51ff3d
Nirbhay <nirbhaysingh943012@gmail.com>
2024-08-19 08:30:02 +0530

    folder changed

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import router from "./routes/routes.js";
+import router from "./src/routes/routes.js";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(
